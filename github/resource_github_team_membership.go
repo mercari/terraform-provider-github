f2d48e39a02df1284bacb54e7ae84405ab7ac1d4
kouzoh-service-account <kouzoh-service-account@users.noreply.github.com>
2020-01-20 10:57:28 +0900

    fix getTeamAndUserFromURL function

--- github/resource_github_team_membership.go
+++ github/resource_github_team_membership.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/google/go-github/v25/github"
+	"github.com/google/go-github/v28/github"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -145,7 +145,7 @@ func getTeamAndUserFromURL(url *string) (string, string) {
 
 	urlSlice := strings.Split(*url, "/")
 	for v := range urlSlice {
-		if urlSlice[v] == "teams" {
+		if urlSlice[v] == "teams" || urlSlice[v] == "team" {
 			team = urlSlice[v+1]
 		}
 		if urlSlice[v] == "memberships" {
